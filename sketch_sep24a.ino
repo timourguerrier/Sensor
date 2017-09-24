b3fe5d8d65c6d7f02e0d82b5bb5a53b27e9e0ae8
timourguerrier <32241995+timourguerrier@users.noreply.github.com>
2017-09-24 20:06:24 +0200

    Add files via upload

---
+++ sketch_sep24a.ino
@@ -0,0 +1,37 @@
+
+int ThermPin = 0;
+int Vo;
+float R1 = 10000;
+float b = 3435;
+float t0 = 273.15;
+float adc = 0;
+float logR,T,R,Tc,Tf,Rth;
+void setup(){
+  Serial.begin(9600);
+}
+float Calcul(float adc)
+{
+  Vo = analogRead(ThermPin);
+  R = (1024/adc - 1);
+  logR = log(R);
+  T= 1.0/ (1/(Vo+t0) + 1/b * logR);
+  Tc = T - 273.15;
+  //Tf = (Tc * 9.0)/ 5.0 + 32.0; 
+  Rth = exp(b * (1/T - 1/(Vo+t0)));
+  
+  return Rth;
+ }
+void loop()
+{
+  while(adc<1025)
+  {
+  Serial.print("Temperature: ");   
+  Serial.print(Tc);
+  Serial.println(" C"); 
+  Serial.print(Calcul(adc));
+  Serial.print("K Rt; "); 
+  delay(1000);
+  adc++;
+  }
+}
+
